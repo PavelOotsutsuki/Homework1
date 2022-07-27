de2daf9754aa61765188b0418ea8db183e365ee5
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-07-27 20:08:47 +0300

    Добавьте файлы проекта.

---
+++ Homework1.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char buttonKeyboard; // Кнопка клавиатуры
+            string cheatCode; // Чит-код
+            byte column; // Количество столбцов в таблице
+            bool isDraw; //Рисовать?
+            short armor; //Броня от 0 до 10000
+            ushort attack; //Атака от 0 до 50000
+            int ballsOnMap; //Количество шаров на карте
+            long usersServer2; // Кол-во игроков на 2-м серевере
+            float degView; // Градус обзора
+            double hp; //Жизни
+
+        }
+    }
+}
